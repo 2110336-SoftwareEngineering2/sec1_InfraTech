205d5aebfd8f76e29dd7e78715d946906712a47c
techid2000 <techid2000@gmail.com>
2021-02-04 23:50:29 +0700

    ✨ add and config typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegisterModule } from './register/register.module';
-
 @Module({
-  imports: [RegisterModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      // TODO: use configs
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'admin',
+      database: 'letx',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    RegisterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
