d7790ecac353e95e61e5b188911b4c92691f6e28
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-08 09:54:59 +0700

    Remove unnecessary initial file

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,8 @@
 import 'antd/dist/antd.css';
-import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default MyApp;
+export default App;
