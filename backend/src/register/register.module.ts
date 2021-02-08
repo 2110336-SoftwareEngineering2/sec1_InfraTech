cdb9f8b3cfacbc260177ddedc26c734fc9f70821
techid2000 <techid2000@gmail.com>
2021-02-08 13:44:03 +0700

    ⚗️ test db using register to create user auth

--- backend/src/register/register.module.ts
+++ backend/src/register/register.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserAuth } from './user-auth.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserAuth])],
   controllers: [RegisterController],
   providers: [RegisterService],
 })
