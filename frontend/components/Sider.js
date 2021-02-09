fb92e5d1c09a7d44f8c325f6f500c657d72a1dd6
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-09 21:00:52 +0700

    Edit menu item to use index as a key

--- frontend/components/Sider.js
+++ frontend/components/Sider.js
@@ -6,9 +6,9 @@ const { Sider: AntdSider } = Layout;
 const Sider = ({ collapsed, userType }) => (
   <AntdSider trigger={null} collapsible collapsed={collapsed}>
     <div className='h-8 bg-gray-400 bg-opacity-30 m-4 mb-8' />
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
       {MENU_ITEMS[userType].map(({ text, icon }, index) => (
-        <Menu.Item key={(index + 1).toString()} icon={icon}>
+        <Menu.Item key={index} icon={icon}>
           {text}
         </Menu.Item>
       ))}
