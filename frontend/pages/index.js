fb169806785683864d16fe7b97c9de1c0eedade3
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-09 14:28:43 +0700

    Add tailwind config

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,11 @@
+import AppLayout from '../components/AppLayout';
+
 const Landing = () => (
-  <div>CONTENT</div>
-)
+  <AppLayout>
+    <div className="min-h-screen flex justify-center pt-40 font-bold text-4xl">
+      Content
+    </div>
+  </AppLayout>
+);
 
 export default Landing;
