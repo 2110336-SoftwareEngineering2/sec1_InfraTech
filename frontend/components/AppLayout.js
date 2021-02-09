5798012d9d485e1119dedd9650b2b26165439fd2
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-09 20:28:20 +0700

    Add mock user type

--- frontend/components/AppLayout.js
+++ frontend/components/AppLayout.js
@@ -4,21 +4,30 @@ import { Layout } from 'antd';
 import Header from './Header';
 import Sider from './Sider';
 import Footer from './Footer';
+import { USER_TYPE } from '../config/UserType.config';
 
 const { Content } = Layout;
 
 const AppLayout = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
   const [isSignIn, setSignIn] = useState(false);
+  const [userType, setUserType] = useState(USER_TYPE.GUEST);
 
   return (
     <Layout>
-      <Sider collapsed={collapsed} userType={'trainer'} />
+      <Sider collapsed={collapsed} userType={userType} />
       <Layout>
         <Header
           isSignIn={isSignIn}
           clickMenu={() => setCollapsed(!collapsed)}
-          clickButton={() => setSignIn(!isSignIn)}
+          clickButton={() => {
+            setSignIn(!isSignIn);
+            if (!isSignIn) {
+              setUserType(USER_TYPE.TRAINER);
+            } else {
+              setUserType(USER_TYPE.GUEST);
+            }
+          }}
         />
         <Content>{children}</Content>
         <Footer />
