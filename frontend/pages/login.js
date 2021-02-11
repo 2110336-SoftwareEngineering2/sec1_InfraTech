0e473e179564ef505dfc64d3a2dea11e0a1ba449
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-11 22:46:07 +0700

    Create login form component

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,49 +1,27 @@
 import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 import Image from 'next/image';
+
+import LoginForm from '../components/LoginForm';
 import Footer from '../components/Footer';
 
 const Login = () => {
   const [form] = Form.useForm();
-  const onFinish = data => console.log(data);
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div>
+    <>
       <div className="bg-gradient-blue-purple flex justify-center">
-        <div className="bg-white p-8 my-40 rounded-lg flex justify-between">
+        <div className="bg-white p-10 my-40 rounded-lg flex justify-between">
           <Image src="/login.svg" width={240} height={300} />
           <div className="mx-12 mt-16 text-4xl font-bold">
             Welcome Back!
-            <Form className="mt-6 w-80" layout={'vertical'} form={form} onFinish={onFinish}>
-              <Form.Item
-                name="email"
-                rules={[{ required: true, message: 'Please input your email' }]}
-              >
-                <Input className="form-text-box" placeholder="Email" />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[
-                  { required: true, message: 'Please input your password' },
-                ]}
-              >
-                <Input
-                  className="form-text-box password"
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Login
-                </Button>
-              </Form.Item>
-            </Form>
+            <LoginForm form={form} onSubmit={onSubmit} />
           </div>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
