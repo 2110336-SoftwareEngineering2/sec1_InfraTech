96e5ad24cbba31e91d92a52a4c58225a12e9e7fa
techid2000 <techid2000@gmail.com>
2021-02-11 14:01:42 +0700

    ✨ implement register service

--- backend/src/register/register.service.ts
+++ backend/src/register/register.service.ts
@@ -4,29 +4,76 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+
 import { RegisterFormDto } from './dtos/register-form-dto';
 import { UserAuth } from '../entities/user-auth.entity';
-import * as bcrypt from 'bcryptjs';
+import { TrainerProfile } from './entities/trainer-profile.entity';
+import { TraineeProfile } from './entities/trainee-profile.entity';
+import { UserType } from './enums/user-type.enum';
 
 @Injectable()
 export class RegisterService {
   constructor(
     @InjectRepository(UserAuth)
     private userAuthRepository: Repository<UserAuth>,
+    @InjectRepository(TrainerProfile)
+    private trainerProfileRepository: Repository<TrainerProfile>,
+    @InjectRepository(TraineeProfile)
+    private traineeProfileRepository: Repository<TraineeProfile>,
+    private connection: Connection,
   ) {}
+
   async register(registerFormDto: RegisterFormDto): Promise<UserAuth> {
+    const { userType } = registerFormDto;
+
+    const userAuth = await this.createUserAuth(registerFormDto);
+    const ProfileEntity =
+      userType === UserType.Trainer ? TrainerProfile : TraineeProfile;
+    const profile =
+      userType === UserType.Trainer
+        ? this.createTrainerProfile(registerFormDto)
+        : this.createTraineeProfile(registerFormDto);
+
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.insert(UserAuth, userAuth);
+      await queryRunner.manager.insert(ProfileEntity, profile);
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('This email already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+    // TODO: return view entity (UserAuth join with Profile)
+    return userAuth;
+  }
+
+  async createUserAuth(registerFormDto: RegisterFormDto): Promise<UserAuth> {
+    const { email, password } = registerFormDto;
     const userAuth = this.userAuthRepository.create();
+    // TODO: refactor duplicate hash logic
+    // #region hash password
     const saltRounds = 10;
 
     await new Promise<void>((resolve, reject) => {
-      bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.genSalt(saltRounds, function (err, salt) {
         if (err) {
           reject(err);
           return;
         }
 
-        bcrypt.hash(registerFormDto.password, salt, function(err, hash) {
+        bcrypt.hash(password, salt, function (err, hash) {
           // Store hash in your password DB.
           if (err) {
             reject(err);
@@ -38,20 +85,64 @@ export class RegisterService {
           resolve();
         });
       });
-    })
+    });
+    //#endregion
 
-    userAuth.email = registerFormDto.email;
-
-    try {
-      await this.userAuthRepository.insert(userAuth);
-    } catch (error) {
-      if (error.code === 'ER_DUP_ENTRY') {
-        throw new ConflictException('This email already exists');
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
-
+    userAuth.email = email;
     return userAuth;
+  }
+
+  createTrainerProfile(registerFormDto: RegisterFormDto): TrainerProfile {
+    const {
+      email,
+      firstname,
+      lastname,
+      cid,
+      gender,
+      birthdate,
+      phoneNumber,
+      profileImageUrl,
+      preferences,
+    } = registerFormDto;
+
+    const profile = this.trainerProfileRepository.create();
+
+    profile.email = email;
+    profile.firstname = firstname;
+    profile.lastname = lastname;
+    profile.cid = cid;
+    profile.gender = gender;
+    profile.birthdate = birthdate;
+    profile.phoneNumber = phoneNumber;
+    profile.profileImageUrl = profileImageUrl;
+    profile.preferences = preferences;
+
+    return profile;
+  }
+
+  createTraineeProfile(registerFormDto: RegisterFormDto): TraineeProfile {
+    const {
+      email,
+      firstname,
+      lastname,
+      gender,
+      birthdate,
+      phoneNumber,
+      profileImageUrl,
+      preferences,
+    } = registerFormDto;
+
+    const profile = this.traineeProfileRepository.create();
+
+    profile.email = email;
+    profile.firstname = firstname;
+    profile.lastname = lastname;
+    profile.gender = gender;
+    profile.birthdate = birthdate;
+    profile.phoneNumber = phoneNumber;
+    profile.profileImageUrl = profileImageUrl;
+    profile.preferences = preferences;
+
+    return profile;
   }
 }
