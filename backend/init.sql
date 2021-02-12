54ccc90bb99c162ef9b516f527d6617e2ab558b2
techid2000 <techid2000@gmail.com>
2021-02-12 18:05:54 +0700

    🐛 fix db schema

--- backend/init.sql
+++ backend/init.sql
@@ -1,7 +1,7 @@
 USE letx;
 
 CREATE TABLE user_auth (
-	id VARCHAR(36) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     salt VARCHAR(255)
@@ -14,13 +14,13 @@ CREATE TABLE trainer_profile (
     gender VARCHAR(10),
     phone_number VARCHAR(15),
     birthdate DATETIME,
-	cid VARCHAR(13),
+    cid VARCHAR(13),
     profile_image_url VARCHAR(255),
     CONSTRAINT FK_trainer_profile_user_id FOREIGN KEY (user_id) REFERENCES user_auth(id) ON DELETE CASCADE
 );
 
 CREATE TABLE trainee_profile (
-    user_id VARCHAR(255),
+    user_id VARCHAR(36),
     firstname VARCHAR(255),
     lastname VARCHAR(255),
     gender VARCHAR(10),
