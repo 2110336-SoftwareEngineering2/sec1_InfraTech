4541d081c4a1fcce3eefe599882fb7ab3c6eedd2
techid2000 <techid2000@gmail.com>
2021-02-12 17:57:47 +0700

    ✨ restructure schema

--- backend/init.sql
+++ backend/init.sql
@@ -1,13 +1,14 @@
 USE letx;
 
 CREATE TABLE user_auth (
-    email VARCHAR(255) PRIMARY KEY,
+	id VARCHAR(36) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     salt VARCHAR(255)
 );
 
 CREATE TABLE trainer_profile (
-    email VARCHAR(255),
+    user_id VARCHAR(36),
     firstname VARCHAR(255),
     lastname VARCHAR(255),
     gender VARCHAR(10),
@@ -15,16 +16,16 @@ CREATE TABLE trainer_profile (
     birthdate DATETIME,
 	cid VARCHAR(13),
     profile_image_url VARCHAR(255),
-    CONSTRAINT FK_trainer_email FOREIGN KEY (email) REFERENCES user_auth(email) ON DELETE CASCADE
+    CONSTRAINT FK_trainer_profile_user_id FOREIGN KEY (user_id) REFERENCES user_auth(id) ON DELETE CASCADE
 );
 
 CREATE TABLE trainee_profile (
-    email VARCHAR(255),
+    user_id VARCHAR(255),
     firstname VARCHAR(255),
     lastname VARCHAR(255),
     gender VARCHAR(10),
     phone_number VARCHAR(15),
     birthdate DATETIME,
     profile_image_url VARCHAR(255),
-    CONSTRAINT FK_trainee_email FOREIGN KEY (email) REFERENCES user_auth(email) ON DELETE CASCADE
+    CONSTRAINT FK_trainee_profile_user_id FOREIGN KEY (user_id) REFERENCES user_auth(id) ON DELETE CASCADE
 );
