9e2a3ceea66a27ffaac54eea074d56839b9e9689
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-13 15:15:48 +0700

    Implement mock user session logic using cookie

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,13 @@
+import { CookiesProvider } from 'react-cookie';
 import 'antd/dist/antd.css';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <CookiesProvider>
+      <Component {...pageProps} />
+    </CookiesProvider>
+  );
 };
 
 export default App;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,31 @@
+import cookie from 'cookie';
 import AppLayout from '../components/AppLayout';
 
-const Landing = () => (
-  <AppLayout>
+const Landing = ({ userInfo }) => (
+  <AppLayout userInfo={userInfo}>
     <div className="min-h-screen flex justify-center pt-40 font-bold text-4xl">
       Content
     </div>
   </AppLayout>
 );
+
+export async function getServerSideProps({ req }) {
+  const data = cookie.parse(
+    req ? req.headers.cookie || '' : document.cookie,
+  );
+
+  // TODO: fetch user info from server
+  const mockUserInfo = {
+    firstname: "John",
+    lastname: "Cena",
+    profile: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmuc.fandom.com%2Fwiki%2FJohn_Cena&psig=AOvVaw2MtR7cbMqHVH2jUJg98g_X&ust=1613284647385000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCIuOef5u4CFQAAAAAdAAAAABAU",
+    userType: "trainer"
+  }
+  const userInfo = data?.user ? mockUserInfo : null
+  
+  return {
+    props: { userInfo },
+  };
+};
 
 export default Landing;
