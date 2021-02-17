df4fa6cd0078b4508299858a39f616f703da44f8
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-02-17 12:11:53 +0700

    clickable card

--- frontend/components/PreferrenceCard.js
+++ frontend/components/PreferrenceCard.js
@@ -1,8 +1,31 @@
+import { useState } from 'react';
 const PreferrenceCard = ({}) => {
+  const [click, setClick] = useState(false);
   return (
     <div
-      className={'w-60 h-72 items-between rounded-md border-2 border-black-500'}
+      className={`w-60 h-72 items-between rounded-md border-2 border-black-500 ${
+        click ? 'ring-4 ring-indigo-300' : ''
+      }`}
+      onClick={() => {
+        setClick(!click);
+      }}
     >
+      <div className="w-full h-0 text-right items-start relative ">
+        {click ? (
+          <CheckCircleOutlined
+            style={{
+              position: 'absolute',
+              color: '#08c',
+              fontSize: '1rem',
+              top: '5px',
+              right: '5px',
+            }}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+
       <div className="w-60 h-60 ">
         <img alt="example" src="a.jpg" class="object-contain h-60 w-60 " />
       </div>
@@ -13,5 +36,3 @@ const PreferrenceCard = ({}) => {
     </div>
   );
 };
-
-export default PreferrenceCard;
