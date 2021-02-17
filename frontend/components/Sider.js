64126f6a2a421987fef81c46151a58075f970704
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-17 20:15:12 +0700

    Add responsive sider on login page

--- frontend/components/Sider.js
+++ frontend/components/Sider.js
@@ -1,19 +1,37 @@
+import { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import { MENU_ITEMS } from '../config/Sider.config';
 
 const { Sider: AntdSider } = Layout;
 
-const Sider = ({ collapsed, userType }) => (
-  <AntdSider trigger={null} collapsible collapsed={collapsed}>
-    <div className='h-8 bg-gray-400 bg-opacity-30 m-4 mb-8' />
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
-      {MENU_ITEMS[userType].map(({ text, icon }, index) => (
-        <Menu.Item key={index} icon={icon}>
-          {text}
-        </Menu.Item>
-      ))}
-    </Menu>
-  </AntdSider>
-);
+const Sider = ({ collapsed, userType }) => {
+  const [isBroken, setIsBroken] = useState(false);
+
+  return (
+    <AntdSider
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+      breakpoint="md"
+      collapsedWidth={isBroken ? 0 : 80}
+      width={isBroken ? 65 : 170}
+      onBreakpoint={(broken) => setIsBroken(broken)}
+    >
+      <div className="h-8 bg-gray-400 bg-opacity-30 m-4 mb-8" />
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['0']}
+        inlineCollapsed={collapsed}
+      >
+        {MENU_ITEMS[userType].map(({ text, icon }, index) => (
+          <Menu.Item key={index} icon={icon} title={text}>
+            {!isBroken && text}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </AntdSider>
+  );
+};
 
 export default Sider;
