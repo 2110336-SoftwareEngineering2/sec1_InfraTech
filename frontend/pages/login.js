d5259e536834c0c342c55d04cc6cfcc7aea2857f
Wachirapat M <wachi_tony@yahoo.co.th>
2021-02-17 15:18:40 +0700

    Apply prettier

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie';
 import { Form } from 'antd';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -10,15 +10,15 @@ import Footer from '../components/Footer';
 // TODO: Implement responsive handling
 const Login = () => {
   const [form] = Form.useForm();
-  const router = useRouter()
-  const [cookie, setCookie] = useCookies(["user"])
+  const router = useRouter();
+  const [cookie, setCookie] = useCookies(['user']);
 
   // TODO: Connect to login API
   const handleSubmit = ({ email, password }) => {
     // console.log({ email, password });
-    setCookie("user", email, {path: "/", maxAge: 3600})
-    router.push('/')
-  }
+    setCookie('user', email, { path: '/', maxAge: 3600 });
+    router.push('/');
+  };
 
   return (
     <>
