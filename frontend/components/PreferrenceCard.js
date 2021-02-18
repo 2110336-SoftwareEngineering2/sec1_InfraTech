86932a7672d055abe5c1d13851f09330f4077a2b
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-02-18 21:25:55 +0700

    square card

--- frontend/components/PreferrenceCard.js
+++ frontend/components/PreferrenceCard.js
@@ -6,7 +6,7 @@ const PreferrenceCard = ({ url, description, alt }) => {
   const [click, setClick] = useState(false);
   return (
     <div
-      className={`w-60 h-64 relative flex flex-col justify-center items-between rounded-md border-2 bg-white ${
+      className={`w-60 h-60 relative flex flex-col justify-center items-between rounded-md pt-2 border-2 bg-white ${
         click ? ' border-blue' : 'border-black'
       }`}
       onClick={() => {
@@ -16,7 +16,7 @@ const PreferrenceCard = ({ url, description, alt }) => {
       {click && (
         <CheckCircleFilled className="absolute text-blue text-base top-3 right-3 z-10" />
       )}
-      <div className="w-full h-52 relative rounded-md z-0 overflow-hidden">
+      <div className="w-full h-48 relative rounded-md z-0 overflow-hidden">
         <Image layout="fill" alt={alt} src={url} />
       </div>
       <div className=" w-full h-12 relative flex rounded-md items-center justify-center font-bold text-xl ">
