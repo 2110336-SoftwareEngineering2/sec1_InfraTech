c9edeffc09623c49f508e299087dd1b4f328a3e3
flumen17 <mark10001971@hotmail.com>
2021-02-21 16:39:56 +0700

    edit selection card

--- frontend/components/SelectionCard.js
+++ frontend/components/SelectionCard.js
@@ -1,21 +1,19 @@
 import { CheckCircleFilled } from '@ant-design/icons';
 import Image from 'next/image';
 
-const PreferrenceCard = ({ flag, click, url, description, alt }) => {
+const PreferrenceCard = ({ flag, onClick, imageUrl, description}) => {
   return (
     <div
       className={`w-60 h-60 relative flex flex-col justify-center items-between rounded-md pt-2 border-2 m-5 bg-white ${
         flag ? ' border-blue' : 'border-gray-300'
       }`}
-      onClick={() => {
-        click();
-      }}
+      onClick={onClick}
     >
       {flag && (
         <CheckCircleFilled className="absolute text-blue text-base top-3 right-3 z-10" />
       )}
       <div className="w-full h-48 relative rounded-md z-0 overflow-hidden">
-        <Image layout="fill" alt={alt} src={url} />
+        <Image layout="fill" alt={description} src={imageUrl} />
       </div>
       <div className=" w-full h-12 relative flex rounded-md items-center justify-center font-bold text-xl ">
         {description}
