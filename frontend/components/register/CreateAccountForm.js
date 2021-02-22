2290a2367f7036a4299f5e771ecc42595d9d010c
flumen17 <mark10001971@hotmail.com>
2021-02-23 05:17:49 +0700

    fix white box in register page width and style create account

--- frontend/components/register/CreateAccountForm.js
+++ frontend/components/register/CreateAccountForm.js
@@ -12,56 +12,53 @@ const CreateAccountForm = ({ getState, setState, size, current, next }) => {
     next();
   };
   return (
-    <div className="flex justify-between">
-      <div className="hidden md:block mt-0 lg:mt-8">
-        <Image src="/register.svg" width={240} height={300} />
-      </div>
-      <div className="mx-8 md:mx-12">
-      <div>
-        Step {current} of {size}
-      </div>
-      <div className="text-3xl sm:text-4xl font-bold">Create Account</div>
-      <Form
-        className="mt-6"
-        form={form}
-        initialValues={getState('create-account', {})}
-        onFinish={onContinue}
-      >
-        <Form.Item name="email" hasFeedback rules={
-          [
-            {type: 'email', message: 'Email format is incorrect'}, 
-            {required: true, message: 'Please provide an email.'}
-          ]
-        }>
-          <Input type="email" placeholder="Email" />
-        </Form.Item>
+    <div className="flex justify-between w-full">
+      <Image src="/register.svg" width={300} height={400}/>
+      <div className="mt-4 w-3/5">
+        <div>
+          Step {current} of {size}
+        </div>
+        <div className="text-3xl sm:text-4xl font-bold">Create Account</div>
+        <Form
+          form={form}
+          initialValues={getState('create-account', {})}
+          onFinish={onContinue}
+        >
+          <Form.Item className="mt-10" name="email" hasFeedback rules={
+            [
+              {type: 'email', message: 'Email format is incorrect'}, 
+              {required: true, message: 'Please provide an email.'}
+            ]
+          }>
+            <Input type="email" placeholder="Email" />
+          </Form.Item>
 
-        <Form.Item name="password" hasFeedback rules={
-          [{required: true, message: 'Please specify password.'}]
-        }>
-          <Input.Password placeholder="Password" />
-        </Form.Item>
+          <Form.Item className="mt-10" name="password" hasFeedback rules={
+            [{required: true, message: 'Please specify password.'}]
+          }>
+            <Input.Password placeholder="Password" />
+          </Form.Item>
 
-        <Form.Item name="confirm" hasFeedback dependencies={['password']} rules={
-          [
-            {required: true, message: 'Please confirm your password.'}, 
-            ({getFieldValue}) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) return Promise.resolve();
-                return Promise.reject('Password does not match.')
-              }
-            })
-          ]
-        }>
-          <Input.Password placeholder="Confirm Password" />
-        </Form.Item>
+          <Form.Item className="mt-10" name="confirm" hasFeedback dependencies={['password']} rules={
+            [
+              {required: true, message: 'Please confirm your password.'}, 
+              ({getFieldValue}) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) return Promise.resolve();
+                  return Promise.reject('Password does not match.')
+                }
+              })
+            ]
+          }>
+            <Input.Password placeholder="Confirm Password" />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="w-24">
-            Continue
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item className="mt-10">
+            <Button type="primary" htmlType="submit" className="w-24">
+              Continue
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
