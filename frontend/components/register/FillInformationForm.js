44843bdfc61458ed576fd241b44d1d2182fd3684
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-02-22 18:42:53 +0700

    add uploadcomponent to fill information page

--- frontend/components/register/FillInformationForm.js
+++ frontend/components/register/FillInformationForm.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Button, DatePicker, Form, Input, InputNumber, Row, Select } from 'antd';
+import React, { useState } from 'react';
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Select,
+  Upload,
+} from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import Image from 'next/image';
-
+import fire from './../../config/firebase';
+import CustomUpload from '../CustomUpload';
 // NOTE: draft version
 const FillInformationForm = ({ getState, setState, size, current, prev }) => {
   const [form] = Form.useForm();
@@ -23,6 +34,7 @@ const FillInformationForm = ({ getState, setState, size, current, prev }) => {
     setState('information', values);
     prev();
   };
+
   return (
     <div>
       <div>
@@ -36,83 +48,116 @@ const FillInformationForm = ({ getState, setState, size, current, prev }) => {
       >
         <div className="flex justify-between">
           <div className="hidden md:block mt-0 lg:mt-10 mx-8">
-            <Image src="/information.svg" width={200} height={200} />
             <Form.Item className="text-center mt-6">
-              <Button className="w-24">Change</Button>
+              <CustomUpload />
             </Form.Item>
           </div>
           <div className="mt-12 ml-12 w-96">
-            <Form.Item name="firstName" hasFeedback rules={
-              [{required: true, message: 'Please provide first name.'}]
-            }>
+            <Form.Item
+              name="firstName"
+              hasFeedback
+              rules={[
+                { required: true, message: 'Please provide first name.' },
+              ]}
+            >
               <Input placeholder="First Name" />
             </Form.Item>
 
-            <Form.Item name="lastName" hasFeedback rules={
-              [{required: true, message: 'Please provide last name.'}]
-            }>
+            <Form.Item
+              name="lastName"
+              hasFeedback
+              rules={[{ required: true, message: 'Please provide last name.' }]}
+            >
               <Input placeholder="Last Name" />
             </Form.Item>
-            
+
             <Row justify="space-between">
-              <Form.Item name="gender" className="w-32" hasFeedback rules={
-                [{required: true, message: 'Gender is not specified'}]
-              }>
+              <Form.Item
+                name="gender"
+                className="w-32"
+                hasFeedback
+                rules={[{ required: true, message: 'Gender is not specified' }]}
+              >
                 <Select placeholder="Gender">
                   <Select.Option value="male">Male</Select.Option>
                   <Select.Option value="female">Female</Select.Option>
                 </Select>
               </Form.Item>
 
-              <Form.Item name="dateOfBirth" className="w-60" hasFeedback rules={
-                [
-                  {type: 'date', message: 'Date format is incorrect.'},
-                  {required: true, message: 'Please select birth date.'}
-                ]
-              }>
-                <DatePicker style={{ width: '100%' }} placeholder="Date of birth" format="DD/MM/YYYY" />
+              <Form.Item
+                name="dateOfBirth"
+                className="w-60"
+                hasFeedback
+                rules={[
+                  { type: 'date', message: 'Date format is incorrect.' },
+                  { required: true, message: 'Please select birth date.' },
+                ]}
+              >
+                <DatePicker
+                  style={{ width: '100%' }}
+                  placeholder="Date of birth"
+                  format="DD/MM/YYYY"
+                />
               </Form.Item>
             </Row>
 
-            <Form.Item name="citizenId" hasFeedback rules={
-              [
-                {required: true, message: 'Citizen ID must be specified.'},
-                {validator: (_, value) => {
-                  // ref: https://snasui.com/wordpress/identification/
+            <Form.Item
+              name="citizenId"
+              hasFeedback
+              rules={[
+                { required: true, message: 'Citizen ID must be specified.' },
+                {
+                  validator: (_, value) => {
+                    // ref: https://snasui.com/wordpress/identification/
 
-                  if (!value || value === "") return Promise.resolve();
-                  if (!/^\d{13}$/.test(value)) return Promise.reject('Citizen ID must has only 13 digits.')
+                    if (!value || value === '') return Promise.resolve();
+                    if (!/^\d{13}$/.test(value))
+                      return Promise.reject(
+                        'Citizen ID must has only 13 digits.',
+                      );
 
-                  // find sum of adjusted first 12 digits
-                  let sum = 0;
-                  for(let i=0; i<12; i++) {
-                    sum += parseInt(value.charAt(i), 10) * (13-i);
-                  }
-                    
-                  // evaluate check digit
-                  if (((11 - (sum % 11)) % 10).toString() === value.charAt(12)) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject('Check digit of citizen ID is incorrect.');
-                }},
-              ]
-            }>
+                    // find sum of adjusted first 12 digits
+                    let sum = 0;
+                    for (let i = 0; i < 12; i++) {
+                      sum += parseInt(value.charAt(i), 10) * (13 - i);
+                    }
+
+                    // evaluate check digit
+                    if (
+                      ((11 - (sum % 11)) % 10).toString() === value.charAt(12)
+                    ) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      'Check digit of citizen ID is incorrect.',
+                    );
+                  },
+                },
+              ]}
+            >
               <Input placeholder="Citizen ID" />
             </Form.Item>
 
-            <Form.Item name="phoneNumber" hasFeedback rules={
-              [
-                {pattern: /^\d{9,10}$/, message: 'Phone number format is incorrect (9 - 10 digits).'},
-                {required: true, message: 'Please provide phone number.'}
-              ]
-            }>
+            <Form.Item
+              name="phoneNumber"
+              hasFeedback
+              rules={[
+                {
+                  pattern: /^\d{9,10}$/,
+                  message: 'Phone number format is incorrect (9 - 10 digits).',
+                },
+                { required: true, message: 'Please provide phone number.' },
+              ]}
+            >
               <Input placeholder="Phone number" />
             </Form.Item>
           </div>
         </div>
 
         <Form.Item className="text-center mt-10">
-          <Button onClick={onBack} className="w-24 mr-4">Back</Button>
+          <Button onClick={onBack} className="w-24 mr-4">
+            Back
+          </Button>
           <Button type="primary" htmlType="submit" className="w-24 ml-4">
             Submit
           </Button>
