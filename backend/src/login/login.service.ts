4c40dadef4b62daeb9ea90a0bd676db3d2c5a8fe
techid2000 <techid2000@gmail.com>
2021-02-22 22:31:54 +0700

    🎨 formatted, fixed lint and removed unused imports

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -1,7 +1,5 @@
 import {
-  ConflictException,
   Injectable,
-  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -9,7 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { LoginFormDto } from './dtos/login-form-dto';
-import * as bcrypt from 'bcryptjs'
+import * as bcrypt from 'bcryptjs';
 import { LoginTokenDto } from './dtos/login-token-dto';
 import * as jwt from 'jsonwebtoken';
 import { Trainer } from 'src/entities/trainer.entity';
@@ -24,10 +22,8 @@ export class LoginService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-
     @InjectRepository(Trainee)
     private traineeRepository: Repository<Trainee>,
-
     @InjectRepository(Trainer)
     private trainerRepository: Repository<Trainer>,
   ) {}
@@ -35,17 +31,21 @@ export class LoginService {
   generateToken(id: string, email: string, type: string): LoginTokenDto {
     const LIFETIME = 1000000; // In seconds
 
-    const token = jwt.sign({
-      sub: id,
-      email: email,
-      type: type,
-      iat: Math.floor(Date.now() / 1000),
-      exp: Math.floor(Date.now() / 1000) + LIFETIME,
-    }, authConfig.jwtSecret);
+    const token = jwt.sign(
+      {
+        sub: id,
+        email: email,
+        type: type,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + LIFETIME,
+      },
+      authConfig.jwtSecret,
+    );
 
     return {
-      email, token
-    }
+      email,
+      token,
+    };
   }
 
   async login(loginFormDto: LoginFormDto): Promise<LoginTokenDto> {
@@ -53,37 +53,41 @@ export class LoginService {
       const user = await this.userRepository.findOneOrFail({
         where: {
           email: loginFormDto.email,
-        }
+        },
       });
 
       await new Promise<void>((resolve, reject) => {
-        bcrypt.compare(loginFormDto.password, user.password, function(err, result) {
-          // result == true
-          if (err) {
-            reject(err);
-            return;
-          }
+        bcrypt.compare(
+          loginFormDto.password,
+          user.password,
+          function (err, result) {
+            // result == true
+            if (err) {
+              reject(err);
+              return;
+            }
 
-          if (result) {
-            resolve();
-          } else {
-            reject(new UnauthorizedException());
-          }
-        });
-      })
+            if (result) {
+              resolve();
+            } else {
+              reject(new UnauthorizedException());
+            }
+          },
+        );
+      });
 
       let type = loginFormDto.type;
 
       if (!type) {
-        let traineeProfile = await this.traineeRepository.findOne({
+        const traineeProfile = await this.traineeRepository.findOne({
           where: {
-            userId: user.id
-          }
+            userId: user.id,
+          },
         });
 
         type = traineeProfile ? UserType.Trainee : UserType.Trainer;
       }
-  
+
       return this.generateToken(user.id, user.email, type);
     } catch (error) {
       throw new NotFoundException();
