5c02b7adf5daf7b2571eb26834ce359ac4136460
techid2000 <techid2000@gmail.com>
2021-02-22 15:36:31 +0700

    ✨ apply appConfig

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,19 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AuthMiddleware } from './middlewares/auth.middleware';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(AuthMiddleware)
+  const appConfig = config.get('app');
 
-  const config = new DocumentBuilder()
+  if(process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  app.use(AuthMiddleware);
+
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('LetX')
     .setDescription('The LetX API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(appConfig.port);
 }
 bootstrap();
