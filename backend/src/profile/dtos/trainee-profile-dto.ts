829d6f5c19f6fcf6f6a12d216ca07540f3b38be9
techid2000 <techid2000@gmail.com>
2021-02-23 00:45:05 +0700

    🎨 add user type for trainer and trainee profile dto

--- backend/src/profile/dtos/trainee-profile-dto.ts
+++ backend/src/profile/dtos/trainee-profile-dto.ts
@@ -1,7 +1,9 @@
 import { Trainee } from 'src/entities/trainee.entity';
 import { Preference } from '../../preference/entities/preference.entity';
+import { UserType } from '../../register/enums/user-type.enum';
 
 export interface TraineeProfileDto extends Trainee {
   email: string;
+  type: UserType;
   preferences: Preference[];
 }
--- backend/src/profile/dtos/trainer-profile-dto.ts
+++ backend/src/profile/dtos/trainer-profile-dto.ts
@@ -1,7 +1,9 @@
 import { Trainer } from 'src/entities/trainer.entity';
 import { Preference } from '../../preference/entities/preference.entity';
+import { UserType } from '../../register/enums/user-type.enum';
 
 export interface TrainerProfileDto extends Trainer {
   email: string;
+  type: UserType;
   preferences: Preference[];
 }
