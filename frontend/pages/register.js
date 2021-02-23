c51461a4c886f8814b7d120084bc8918ffa0355f
Rock <nattanon.hu@gmail.com>
2021-02-24 01:30:24 +0700

    💄 style padding

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -13,7 +13,7 @@ const Register = () => {
   return (
     <>
       <div className="bg-gradient-blue-purple lg:py-10">
-        <div className="bg-white mx-auto w-full min-h-screen px-4 sm:px-6 xl:px-8 pt-10 md:p-10 lg:w-800 lg:h-full lg:min-h-full">
+        <div className="bg-white mx-auto w-full min-h-screen p-4 sm:p-6 md:p-8 xl:p-10 lg:w-800 lg:h-full lg:min-h-full">
           <Steps>
             <Step component={CreateAccountForm} />
             <Step component={SelectRoleForm} />
