2a2fbd3c38104af21cee8aaa151e0ffc37e90dbb
Rock <nattanon.hu@gmail.com>
2021-02-23 19:29:54 +0700

    🥅 handle error when initiate firebase more than multiple times

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -7,12 +7,9 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSEGINGSENDID,
   appId: process.env.NEXT_PUBLIC_APPID,
 };
-try {
+
+if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-} catch (err) {
-  if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack);
-  }
 }
-const fire = firebase;
-export default fire;
+
+export { storage, firebase as default };
