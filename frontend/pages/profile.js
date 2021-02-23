553eb85c63cb12d5ba57a6cbd56f55baf7c9aabd
flumen17 <mark10001971@hotmail.com>
2021-02-23 18:20:17 +0700

    add profile page layout

---
+++ frontend/pages/profile.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import useUser from '../lib/useUser';
+import AppLayout from '../components/AppLayout';
+import EditProfile from '../components/EditProfile';
+import InformationProfile from '../components/InformationProfile';
+
+const Profile = () => {
+  const { user = {}, mutateUser } = useUser();
+  const [isEditing, setIsEditing] = useState(false);
+  const mock = {
+      id: "23433bertgerb33g4ry",
+      email: "test@gmail.com",
+      type: "TRAINER",
+      profile: {
+          userId: "asefernvo3tn4th",
+          firstname: "Jason",
+          lastname: "Youngku",
+          cid: "1234567891011",
+          gender: "male",
+          birthdate: "08/08/2000",
+          phoneNumber: "123412341",
+          profileImageUrl: "/login.svg"
+      }
+  }
+  console.log(mock)
+
+  return (
+    <AppLayout user={user} mutateUser={mutateUser}>
+      <div className="min-h-screen flex justify-center">
+        <div className="bg-white w-full m-12 py-12 px-16 rounded-lg">
+          <div className="text-4xl sm:text-5xl font-bold">{isEditing ? "Edit Profile" : "Profile"}</div>
+          {
+            isEditing ? 
+            <EditProfile profile={mock.profile} setIsEditing={setIsEditing} /> : 
+            <InformationProfile email={mock.email} profile={mock.profile} setIsEditing={setIsEditing}/>
+          }
+        </div>
+      </div>
+    </AppLayout>
+  );
+};
+
+export default Profile;
