b1dca339198f98db4101ed59afd89916d714d7af
techid2000 <techid2000@gmail.com>
2021-02-23 11:10:52 +0700

    ✨ add patch /profile/trainer

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Req, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Req,
+  Get,
+  UseGuards,
+  Patch,
+  SetMetadata,
+  Body,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { LetXRequest } from 'src/middlewares/auth.middleware';
 import { AuthGuard } from '../guards/auth.guard';
 import { TrainerProfileDto } from './dtos/trainer-profile-dto';
 import { TraineeProfileDto } from './dtos/trainee-profile-dto';
-
+import { UserType } from '../register/enums/user-type.enum';
+import { RoleGuard } from '../guards/role.guard';
+import { UpdateTrainerProfileDto } from './dtos/update-trainer-profile-dto';
+import { Role } from '../decorators/role.decorator';
 @Controller('profile')
 export class ProfileController {
   constructor(private profileService: ProfileService) {}
@@ -15,5 +26,18 @@ export class ProfileController {
     @Req() request: LetXRequest,
   ): Promise<TrainerProfileDto | TraineeProfileDto> {
     return this.profileService.getProfileFromRequest(request);
+  }
+
+  @Patch('trainer')
+  @Role(UserType.Trainer)
+  @UseGuards(AuthGuard, RoleGuard)
+  async updateTrainerProfile(
+    @Req() request: LetXRequest,
+    @Body() updateTrainerProfileDto: UpdateTrainerProfileDto,
+  ): Promise<TrainerProfileDto> {
+    return this.profileService.updateTrainerProfile(
+      request,
+      updateTrainerProfileDto,
+    );
   }
 }
