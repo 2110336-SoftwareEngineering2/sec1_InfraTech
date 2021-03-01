8d15e09ba33979ad4b5f157a76703e9cec87877b
Wachirapat M <wachi_tony@yahoo.co.th>
2021-03-01 21:12:05 +0700

    Format profile page

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import moment from 'moment';
+
 import useUser from '../lib/useUser';
 import AppLayout from '../components/AppLayout';
 import EditProfile from '../components/EditProfile';
 import InformationProfile from '../components/InformationProfile';
-import moment from 'moment'
 
 const Profile = () => {
   const { user = {}, mutateUser } = useUser({ redirectTo: '/login' });
@@ -14,13 +15,15 @@ const Profile = () => {
   return (
     <AppLayout user={user} mutateUser={mutateUser}>
       <div className="min-h-screen flex justify-center">
-        <div className="bg-white w-full m-12 py-12 px-24 rounded-lg">
-          <div className="text-4xl sm:text-5xl font-bold">{isEditing ? "Edit Profile" : "Profile"}</div>
-          {
-            isEditing ?
-              <EditProfile profile={user} setIsEditing={setIsEditing} /> :
-              <InformationProfile profile={user} setIsEditing={setIsEditing} />
-          }
+        <div className="bg-white w-full mx-8 mt-8 py-12 px-24">
+          <div className="text-4xl font-bold">
+            {isEditing ? 'Edit Profile' : 'Profile'}
+          </div>
+          {isEditing ? (
+            <EditProfile profile={user} setIsEditing={setIsEditing} />
+          ) : (
+            <InformationProfile profile={user} setIsEditing={setIsEditing} />
+          )}
         </div>
       </div>
     </AppLayout>
