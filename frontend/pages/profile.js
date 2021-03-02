215d2baca8f0ac70654d5b584a3b523d1af7a37f
Wachirapat M <wachi_tony@yahoo.co.th>
2021-03-02 20:38:20 +0700

    Implement profile loading if user is not ready

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -6,30 +6,37 @@ import { AppLayout } from '../components/common';
 import EditProfile from '../components/EditProfile';
 import InformationProfile from '../components/InformationProfile';
 import TrainerCourseList from '../components/course/TrainerCourseList';
+import Loading from '../components/common/Loading';
 
 const Profile = () => {
-  const { user = {}, mutateUser } = useUser({ redirectTo: '/login' });
+  const { user, mutateUser } = useUser({ redirectTo: '/login' });
   const [isEditing, setIsEditing] = useState(false);
 
-  user.birthdate = moment(user.birthdate);
+  if (user) {
+    user.birthdate = moment(user.birthdate);
+  }
 
   return (
     <AppLayout user={user} mutateUser={mutateUser} selectedMenu="2">
-      <div className="min-h-screen flex justify-center">
-        <div className="bg-white w-full mx-8 mt-8 py-12 px-24">
-          <div className="text-4xl font-bold">
-            {isEditing ? 'Edit Profile' : 'Profile'}
+      {!user ? (
+        <Loading />
+      ) : (
+        <div className="min-h-screen flex justify-center">
+          <div className="bg-white w-full mx-8 mt-8 py-12 px-24">
+            <div className="text-4xl font-bold">
+              {isEditing ? 'Edit Profile' : 'Profile'}
+            </div>
+            {isEditing ? (
+              <EditProfile profile={user} setIsEditing={setIsEditing} />
+            ) : (
+              <InformationProfile profile={user} setIsEditing={setIsEditing} />
+            )}
+            <hr className="my-16" />
+            <div className="text-4xl font-bold mb-10">My Courses</div>
+            <TrainerCourseList />
           </div>
-          {isEditing ? (
-            <EditProfile profile={user} setIsEditing={setIsEditing} />
-          ) : (
-            <InformationProfile profile={user} setIsEditing={setIsEditing} />
-          )}
-          <hr className="my-16" />
-          <div className="text-4xl font-bold mb-10">My Courses</div>
-          <TrainerCourseList />
         </div>
-      </div>
+      )}
     </AppLayout>
   );
 };
