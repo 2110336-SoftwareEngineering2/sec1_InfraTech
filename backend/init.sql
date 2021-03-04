95dc44ac3b42fed5ffa90d503ef560c7dc669ba5
techid2000 <techid2000@gmail.com>
2021-03-04 17:45:46 +0700

    ✨ add table review

--- backend/init.sql
+++ backend/init.sql
@@ -48,6 +48,16 @@ CREATE TABLE user_preference(
     CONSTRAINT FK_user_preference_preference_id FOREIGN KEY (preference_id) REFERENCES preference(id) ON DELETE CASCADE
 );
 
+CREATE TABLE review(
+    id VARCHAR(36) PRIMARY KEY,
+    trainee_user_id VARCHAR(36),
+    trainer_user_id VARCHAR(36),
+    comment VARCHAR(1000),
+    rating TINYINT UNSIGNED,
+    CONSTRAINT FK_review_trainee_user_id FOREIGN KEY (trainee_user_id) REFERENCES trainee(user_id) ON DELETE CASCADE,
+    CONSTRAINT FK_review_trainer_user_id FOREIGN KEY (trainer_user_id) REFERENCES trainer(user_id) ON DELETE CASCADE
+);
+
 -- Mock User --
 INSERT INTO user VALUES ("38a04ba7-096f-4af3-abb2-e38a518a01f7", "tanboi@lnwzamail.com", "$2a$10$XPTfy6sx.TUnze7fHhP6XOWds8bdQaS2NTaELcIKHcyqwNPBKtQk6", "$2a$10$XPTfy6sx.TUnze7fHhP6XO");
 INSERT INTO trainer VALUES ("38a04ba7-096f-4af3-abb2-e38a518a01f7", "Somlux", "Kamsing", "MALE", "081234567", "2017-06-15 00:00:00", "0", "https://www.aceshowbiz.com/images/photo/john_cena.jpg");
