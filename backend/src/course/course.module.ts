ccccc9b8cc96b7765dde6b86eca7404448f259ef
Chomtana <Chomtana001@gmail.com>
2021-03-12 22:52:02 +0700

    Draft course CRUD

---
+++ backend/src/course/course.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { CourseController } from './course.controller';
+import { CourseService } from './course.service';
+import { Trainer } from 'src/entities/trainer.entity';
+import { Trainee } from 'src/entities/trainee.entity';
+import { Preference } from '../preference/entities/preference.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Trainer, Trainee, Preference])],
+  controllers: [CourseController],
+  providers: [CourseService],
+})
+export class CourseModule {}
