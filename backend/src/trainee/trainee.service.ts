5cb196dd7aa46377af0a67f8485cebbdefcaaeec
wongtawan <wongtawan.wrk@gmail.com>
2021-03-13 17:57:17 +0700

    complete trainee

--- backend/src/trainee/trainee.service.ts
+++ backend/src/trainee/trainee.service.ts
@@ -6,6 +6,12 @@ import { User } from '../entities/user.entity';
 import { Trainee } from '../entities/trainee.entity';
 import { Builder } from 'builder-pattern';
 import { RegisterFormDto } from '../register/dtos/register-form-dto';
+import { UserService } from '../user/user.service';
+
+interface RegistrationInfo {
+  user: User;
+  registerFormDto: RegisterFormDto;
+}
 
 @Injectable()
 export class TraineeService {
@@ -14,19 +20,20 @@ export class TraineeService {
     private traineeRepository: Repository<Trainee>,
   ) {}
 
-  async createWithRegistrationInfo(
-    user: User,
-    registrationInfo: RegisterFormDto,
-  ): Promise<Trainee> {
-    const traineeBuilder = Builder(Trainee)
+  async createWithRegistrationInfo({
+    user,
+    registerFormDto,
+  }: RegistrationInfo): Promise<Trainee> {
+    const trainee = Builder(Trainee)
       .user(user)
-      .firstname(registrationInfo.firstname)
-      .lastname(registrationInfo.lastname)
-      .gender(registrationInfo.gender)
-      .birthdate(registrationInfo.birthdate)
-      .phoneNumber(registrationInfo.phoneNumber)
-      .profileImageUrl(registrationInfo.profileImageUrl);
+      .firstname(registerFormDto.firstname)
+      .lastname(registerFormDto.lastname)
+      .gender(registerFormDto.gender)
+      .birthdate(registerFormDto.birthdate)
+      .phoneNumber(registerFormDto.phoneNumber)
+      .profileImageUrl(registerFormDto.profileImageUrl)
+      .build();
 
-    return traineeBuilder.build();
+    return trainee;
   }
 }
