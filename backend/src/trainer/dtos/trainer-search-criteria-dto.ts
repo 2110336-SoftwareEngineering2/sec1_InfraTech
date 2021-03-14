c9628c6537cdba032ab2c29f4f3ba2e5e02565aa
techid2000 <techid2000@gmail.com>
2021-03-14 22:21:32 +0700

    ✨ add sortType and limit for dto

--- backend/src/trainer/dtos/trainer-search-criteria-dto.ts
+++ backend/src/trainer/dtos/trainer-search-criteria-dto.ts
@@ -1,5 +1,8 @@
 import { TrainerSortBy } from '../enums/trainer-sort-by.enum';
+import { TrainerSortType } from '../enums/trainer-sort-type.enum';
 export class TrainerSearchCriteriaDto {
   preferences: string[];
   sortBy: TrainerSortBy;
+  sortType: TrainerSortType;
+  limit: number;
 }
