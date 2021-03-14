ad8d2c9fc7e6402b3e7fb05a01982f56cd08d429
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-03-14 23:13:57 +0700

    edit otherViewProfile Page different to my course page and function depend on who are viewer  (Trainee or other trainer)

--- frontend/pages/otherViewProfile.js
+++ frontend/pages/otherViewProfile.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 
 import useUser from '../lib/useUser';
 import { AppLayout } from '../components/common';
-import EditProfile from '../components/EditProfile';
+
 import InformationProfile from '../components/InformationProfile';
 
 import Loading from '../components/common/Loading';
@@ -11,8 +11,8 @@ import { USER_TYPE } from '../config/UserType.config';
 import TrainerCourseListInOtherview from '../components/courseInOtherview/TrainerCourseListInOtherview';
 
 const otherViewProfile = () => {
+  //TODO:แก้จากเป็น user ตัวเองเป็น user ของ trainer ที่ลิ้งตาม ID ของ trainer
   const { user, mutateUser } = useUser({ redirectTo: '/login' });
-  const [isEditing, setIsEditing] = useState(false);
 
   if (user) {
     user.birthdate = moment(user.birthdate);
@@ -23,14 +23,14 @@ const otherViewProfile = () => {
       {user ? (
         <div className="min-h-screen flex justify-center">
           <div className="bg-white w-full mx-8 mt-8 py-12 px-24">
-            <InformationProfile
-              profile={user}
-              setIsEditing={setIsEditing}
-              ownView={false}
-            />
+            <div className="text-4xl font-bold mb-10">Trainer's Profile</div>
+            <InformationProfile profile={user} ownView={false} />
             <hr className="my-16" />
             <div className="text-4xl font-bold mb-10">Trainer's Courses</div>
-            <TrainerCourseListInOtherview type={USER_TYPE.TRAINER} />
+            <TrainerCourseListInOtherview
+              type={user.type}
+              showTrainerName={false}
+            />
           </div>
         </div>
       ) : (
