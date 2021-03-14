2a58d28bdea56aca2d2dbbaa61844d7771566a31
techid2000 <techid2000@gmail.com>
2021-03-14 22:56:10 +0700

    ✏️ fix update_trainer_rating to be update_trainer_average_rating

--- backend/init.sql
+++ backend/init.sql
@@ -60,7 +60,7 @@ CREATE TABLE review(
 );
 
 DELIMITER //
-CREATE PROCEDURE update_trainer_rating(IN trainer_user_id VARCHAR(36))
+CREATE PROCEDURE update_trainer_average_rating(IN trainer_user_id VARCHAR(36))
 BEGIN
     UPDATE trainer SET average_rating = (
 		SELECT CAST(SUM(review.rating)/COUNT(review.id) AS DECIMAL(3,2))
@@ -74,7 +74,7 @@ DELIMITER //
 CREATE TRIGGER trigger_review_insert AFTER INSERT ON review
 FOR EACH ROW
 BEGIN
-	CALL update_trainer_rating(NEW.trainer_user_id);
+	CALL update_trainer_average_rating(NEW.trainer_user_id);
 END //
 DELIMITER ;
 
@@ -82,7 +82,7 @@ DELIMITER //
 CREATE TRIGGER trigger_review_update AFTER UPDATE ON review
 FOR EACH ROW
 BEGIN
-	CALL update_trainer_rating(NEW.trainer_user_id);
+	CALL update_trainer_average_rating(NEW.trainer_user_id);
 END //
 DELIMITER ;
 
@@ -90,7 +90,7 @@ DELIMITER //
 CREATE TRIGGER trigger_review_delete AFTER DELETE ON review
 FOR EACH ROW
 BEGIN
-	CALL update_trainer_rating(OLD.trainer_user_id);
+	CALL update_trainer_average_rating(OLD.trainer_user_id);
 END //
 DELIMITER ;
 
