e47337050597d6610b37a8784c5bd9d2e7ee3f22
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-03-14 23:11:27 +0700

    edit view of trainer course Item when use in trainee course page and in otherview page

--- frontend/components/courseInOtherview/TrainerCourseItemInOtherview.js
+++ frontend/components/courseInOtherview/TrainerCourseItemInOtherview.js
@@ -4,35 +4,51 @@ import {
   DollarCircleOutlined,
   RadarChartOutlined,
   DashboardOutlined,
-  EditOutlined,
-  DeleteOutlined,
   CheckSquareOutlined,
   SyncOutlined,
 } from '@ant-design/icons';
 import { USER_TYPE } from '../../config/UserType.config';
 
 // TODO: Implement onClick for edit and delete icon
-const TrainerCourseItemInOtherview = ({ course, ownView, type }) => {
+const TrainerCourseItemInOtherview = ({ course, type, showTrainerName }) => {
   return (
     <div className="p-6 shadow-around mb-4">
       <div className="mb-6 flex justify-between">
-        <span className=" text-blue font-bold text-xl">{course.title}</span>
+        <div className="mb-6 flex justify-start">
+          <div className=" ml-3 text-blue font-bold text-xl">
+            {course.title}
+          </div>
+          {showTrainerName && (
+            <>
+              <div className=" ml-3 text-black font-bold text-xl">
+                by trainer
+              </div>
+              <div className=" ml-3 text-red-500 font-bold text-xl">
+                {course.trainer.name}
+              </div>
+            </>
+          )}
+        </div>
         {type != USER_TYPE.TRAINER && (
           <div className="text-lg text-gray-400">
-            {course.status == 'noregisted' ? (
+            {course.status == 'register' ? (
               <Button className="ml-6" type="primary" primary>
                 register
               </Button>
             ) : (
               <>
-                {course.status == 'accepted' ? (
+                {!showTrainerName && (
                   <>
-                    already register <CheckSquareOutlined className="m-2" />
-                  </>
-                ) : (
-                  <>
-                    waiting for accept
-                    <SyncOutlined className="m-2" />
+                    {course.status == 'success' ? (
+                      <>
+                        already register <CheckSquareOutlined className="m-2" />
+                      </>
+                    ) : (
+                      <>
+                        waiting for accept
+                        <SyncOutlined className="m-2" />
+                      </>
+                    )}
                   </>
                 )}
                 <Button className="ml-6" type="primary" danger>
