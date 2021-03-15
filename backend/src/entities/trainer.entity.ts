d21bd3df8b8311331c2587f114043464f04c7bad
techid2000 <techid2000@gmail.com>
2021-03-15 23:12:46 +0700

    🎨 add expose for serialization

--- backend/src/entities/trainer.entity.ts
+++ backend/src/entities/trainer.entity.ts
@@ -10,43 +10,80 @@ import { User } from './user.entity';
 import { Review } from '../trainer/entities/review.entity';
 import { Expose } from 'class-transformer';
 import { Course } from '../course/entities/course.entity';
+import { TrainerUseCases } from '../trainer/enums/trainer-use-cases.enum';
 @Entity({ name: 'trainer' })
 export class Trainer {
   @PrimaryColumn({ name: 'user_id' })
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   userId: string;
 
   @Column()
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   firstname: string;
 
   @Column()
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   lastname: string;
 
   @Column()
+  @Expose({ groups: [] })
   cid: string;
 
   @Column()
+  @Expose({ groups: [TrainerUseCases.GetTrainerById] })
   gender: string;
 
   @Column()
+  @Expose({ groups: [TrainerUseCases.GetTrainerById] })
   birthdate: string;
 
   @Column({ name: 'phone_number' })
+  @Expose({ groups: [TrainerUseCases.GetTrainerById] })
   phoneNumber: string;
 
   @Column({ name: 'profile_image_url' })
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   profileImageUrl: string;
 
-  @OneToMany(() => Course, course => course.trainer)
+  @OneToMany(() => Course, (course) => course.trainer)
+  @Expose({ groups: [] })
   courses: Course[];
 
   @OneToOne(() => User, (user) => user.id, { primary: true })
   @JoinColumn({ name: 'user_id' })
+  @Expose({ groups: [TrainerUseCases.GetTrainerByPreferences] })
   user: User;
 
-  @Expose({ groups: [] })
   @OneToMany(() => Review, (review) => review.trainer)
+  @Expose({ groups: [] })
   reviews: Review[];
 
   @Column({ name: 'average_rating' })
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   averageRating: number;
 }
