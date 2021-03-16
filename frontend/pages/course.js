52762a911aa23993c0a8bc40e565e8b74dd60735
pongsatorn 6131327121 <chain011@yahoo.co.th>
2021-03-16 16:16:04 +0700

    connect mycourse to application api

--- frontend/pages/course.js
+++ frontend/pages/course.js
@@ -1,34 +1,58 @@
 import { AppLayout } from '../components/common';
+import axios from 'axios';
 import useUser from '../lib/useUser';
+import { useCookies } from 'react-cookie';
 
-import TrainerCourseListInOtherview from '../components/courseInOtherview/TrainerCourseListInOtherview';
 import Loading from '../components/common/Loading';
 import { USER_TYPE } from '../config/UserType.config';
+import { API_HOST, COOKIE_NAME } from '../config/config';
+import useSWR from 'swr';
+import TraineeApplicationList from '../components/application/TraineeApplicationList';
 
 const Course = () => {
   const { user, mutateUser } = useUser({ redirectTo: '/login' });
+  const [token] = useCookies([COOKIE_NAME]);
+  const { data: app } = useSWR(
+    [`${API_HOST}/application`, token],
+    async (url, token) => {
+      if (!token[COOKIE_NAME]) return;
+
+      const res = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token[COOKIE_NAME] || ''}`,
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return res?.data ?? {};
+    },
+  );
 
   return (
     <AppLayout user={user} mutateUser={mutateUser}>
       {user ? (
         <div className="min-h-screen flex justify-center">
-          <div className="bg-white w-full mx-8 mt-8 py-12 px-24">
+          <div className="bg-white text-5xl w-full mx-8 mt-8 py-12 px-24">
+            My Course
             <hr className="my-16" />
             <div className="text-4xl font-bold m-10">
-              My Arleady Registed Courses
+              My Arleady Approve Courses
             </div>
-            <TrainerCourseListInOtherview
-              type={USER_TYPE.TRAINEE}
-              filter="success"
-              showTrainerName={true}
-            />
+            <TraineeApplicationList filter="approve" app={app} />
             <div className="text-4xl font-bold m-10">
-              Pending For Accept Courses
+              Pending For Approve Courses
             </div>
-            <TrainerCourseListInOtherview
+            <TraineeApplicationList filter="pending" app={app} />
+            <div className="text-4xl font-bold m-10">Complete Courses</div>
+            <TraineeApplicationList
+              className="bg-green-300"
+              filter="complete"
+              app={app}
+            />
+            <div className="text-4xl font-bold m-10">Rejected Courses</div>
+            <TraineeApplicationList
               type={USER_TYPE.TRAINEE}
-              filter="pending"
-              showTrainerName={true}
+              filter="reject"
+              app={app}
             />
           </div>
         </div>
