2101d818adc6e0016a59f4895faab84738b59dd5
techid2000 <techid2000@gmail.com>
2021-03-17 21:32:31 +0700

    👌 add serialization for preference

--- backend/src/preference/entities/preference.entity.ts
+++ backend/src/preference/entities/preference.entity.ts
@@ -1,13 +1,30 @@
+import { Expose } from 'class-transformer';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TrainerUseCases } from '../../trainer/enums/trainer-use-cases.enum';
 
 @Entity({ name: 'preference' })
 export class Preference {
   @PrimaryGeneratedColumn('uuid')
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   id: string;
 
   @Column()
+  @Expose({
+    groups: [
+      TrainerUseCases.GetTrainerByPreferences,
+      TrainerUseCases.GetTrainerById,
+    ],
+  })
   name: string;
 
   @Column({ name: 'svg_url' })
+  @Expose({
+    groups: [],
+  })
   svgUrl: string;
 }
