949771b568924f7bb972fc7f3f80c77868e7f792
flumen17 <mark10001971@hotmail.com>
2021-03-17 22:32:49 +0700

    edit profile

--- frontend/components/InformationProfile.js
+++ frontend/components/InformationProfile.js
@@ -3,7 +3,7 @@ import { Image, Tag } from 'antd';
 import CustomUpload from './CustomUpload';
 import { USER_TYPE } from '../config/UserType.config';
 
-const InformationProfile = ({ profile, ownView }) => {
+const InformationProfile = ({ profile, ownView = true }) => {
   const calculateAge = (birthdate) => {
     const date = new Date(birthdate);
     var ageDifMs = Date.now() - date.getTime();
@@ -14,11 +14,7 @@ const InformationProfile = ({ profile, ownView }) => {
   return (
     <div className="flex mt-10">
       <div className="mr-16 text-center">
-        {ownView ? (
-          <CustomUpload value={profile.profileImageUrl} disable={true} />
-        ) : (
-          <Image src={profile.profileImageUrl} />
-        )}
+        <CustomUpload value={profile.profileImageUrl} disable={true} />
       </div>
       <div className="w-3/5 text-lg mt-2">
         <div className="text-2xl font-bold">{`${profile.firstname} ${profile.lastname}`}</div>
@@ -28,18 +24,23 @@ const InformationProfile = ({ profile, ownView }) => {
           {calculateAge(profile.birthdate)} years old
         </div>
         <div className="mt-4 flex ">
-          {profile.type === USER_TYPE.TRAINER ? 'Expert in ' : 'Interested in '}
-          {profile.preferences &&
-            profile.preferences.map((preference) => (
-              <Tag className="mx-2">{preference.name}</Tag>
-            ))}
+          {!ownView || profile.type === USER_TYPE.TRAINER
+            ? 'Expert in '
+            : 'Interested in '}
+          {ownView
+            ? profile.preferences &&
+              profile.preferences.map((preference) => (
+                <Tag className="mx-2">{preference.name}</Tag>
+              ))
+            : profile.user.preferences &&
+              profile.user.preferences.map((preference) => (
+                <Tag className="mx-2">{preference.name}</Tag>
+              ))}
         </div>
-        {profile.type === USER_TYPE.TRAINER && (
-          <div className="mt-4">Tel: {profile.phoneNumber}</div>
-        )}
-        {profile.type === USER_TYPE.TRAINER && (
-          <div className="mt-4">Email: {profile.email} </div>
-        )}
+        <div className="mt-4">Tel: {profile.phoneNumber}</div>
+        <div className="mt-4">
+          Email: {ownView ? profile.email : profile.user.email}{' '}
+        </div>
       </div>
     </div>
   );
