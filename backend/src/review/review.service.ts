f856dae63bfc90dca27f73bfc4b17ae34dcfbddb
techid2000 <techid2000@gmail.com>
2021-03-25 22:58:21 +0700

    ✨ implement createReview

--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,16 +1,71 @@
-import { Injectable } from '@nestjs/common';
-import { CreateReviewDto } from './dtos/create-review-dto';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Review } from './entities/review.entity';
+import {
+  Application,
+  ApplicationStatus,
+} from '../application/entities/application.entity';
+import { CreateReviewDto } from './dtos/create-review-dto';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
 export class ReviewService {
+  constructor(
+    @InjectRepository(Review)
+    private reviewRepository: Repository<Review>,
+    @InjectRepository(Application)
+    private applicationRepository: Repository<Application>,
+  ) {}
+
   async createReview(
     traineeUserId: string,
     createReviewDto: CreateReviewDto,
   ): Promise<Review> {
-    console.log(traineeUserId);
-    console.log(createReviewDto);
-    return Promise.resolve(null);
+    const { applicationId, comment, rating } = createReviewDto;
+
+    const application = await this.applicationRepository.findOne(
+      applicationId,
+      { relations: ['trainee', 'course', 'course.trainer'] },
+    );
+
+    if (!application) {
+      throw new NotFoundException('The application was not found');
+    }
+    if (traineeUserId !== application.traineeUserId) {
+      throw new BadRequestException(
+        `The trainee doesn't match with one in the application`,
+      );
+    }
+    if (application.status !== ApplicationStatus.COMPLETE) {
+      throw new ForbiddenException(`The application's status was not complete`);
+    }
+
+    const review = this.reviewRepository.create();
+
+    review.trainer = application.course.trainer;
+    review.trainee = application.trainee;
+    review.application = application;
+    review.rating = rating;
+    review.comment = comment;
+
+    try {
+      // TODO: Add serialization for response
+      return await this.reviewRepository.save(review);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new BadRequestException(
+          'The review of appliation already exists',
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   async getReviewsByTrainerId(
