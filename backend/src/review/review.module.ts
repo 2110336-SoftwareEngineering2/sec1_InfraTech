260c622f63ea293376cd21b9d4bb9d2e87f7a74c
techid2000 <techid2000@gmail.com>
2021-03-25 22:57:58 +0700

    ✨ add typeorm feature for review and application

--- backend/src/review/review.module.ts
+++ backend/src/review/review.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { Application } from '../application/entities/application.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review, Application])],
   providers: [ReviewService],
   controllers: [ReviewController],
 })
