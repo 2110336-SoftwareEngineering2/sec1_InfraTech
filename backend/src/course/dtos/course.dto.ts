a15e580e0be6db00ddb3fdb52a5a4669df874c66
Wachirapat M <wachi_tony@yahoo.co.th>
2021-03-26 14:19:15 +0700

    Add api property to course and login form dto

--- backend/src/course/dtos/course.dto.ts
+++ backend/src/course/dtos/course.dto.ts
@@ -1,11 +1,30 @@
-export interface CourseDto {
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CourseDto {
+  @ApiPropertyOptional()
   id?: string;
+
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   description: string;
+
+  @ApiProperty()
   level: string;
+
+  @ApiProperty()
   specialization: string;
+
+  @ApiProperty()
   price: number;
+
+  @ApiProperty()
   period: number;
+
+  @ApiProperty()
   district: string;
+
+  @ApiProperty()
   province: string;
 }
