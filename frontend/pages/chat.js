7a34390f0c07bff22cf2893c0f480a25152f32d7
Chomtana <Chomtana001@gmail.com>
2021-04-02 22:27:00 +0700

    Add chat message layout

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -9,49 +9,47 @@ import { TrainerSortBy, TrainerSortType } from '../config/FilterTrainer.config';
 import { API_HOST } from '../config/config';
 import axios from 'axios';
 
+function ChatItem({message, avatar, bySelf}) {
+  return (
+    <div className={"flex " + (bySelf ? "justify-end" : "")}>
+      <div className={"flex " + (bySelf ? "justify-end" : "")} style={{flexBasis: "75%"}}>
+        <div></div>
+        <div className="p-2 bg-gray-200 rounded-xl">{message}</div>
+      </div>
+    </div>
+  )
+}
+
 const Chat = () => {
   const { user, mutateUser } = useUser({});
-  const [preferenceFilter, setPreferenceFilter] = useState([]);
-  const [sortBy, setSortBy] = useState(TrainerSortBy.AverageRating);
-  const [sortType, setSortType] = useState(TrainerSortType.Ascending);
-  const [trainerList, setTrainerList] = useState([]);
-
-  const getTrainerList = async () => {
-    const res = await axios.post(`${API_HOST}/trainer/preferences`, {
-      preferences: preferenceFilter,
-      sortBy: sortBy,
-      sortType: sortType,
-    });
-    setTrainerList(res.data);
-  };
-
-  useEffect(() => {
-    user &&
-      setPreferenceFilter(user.preferences.map((preference) => preference.id));
-  }, [user]);
-
-  useEffect(() => {
-    getTrainerList();
-  }, [preferenceFilter, sortBy, sortType]);
+  const [messages, setMessages] = useState([
+    {
+      message: "hello world",
+      avatar: "/avatar.svg",
+      bySelf: true
+    },
+    {
+      message: "สวัสดีคนไทย",
+      avatar: "/avatar.svg",
+      bySelf: false
+    },
+    {
+      message: "O_O",
+      avatar: "/avatar.svg",
+      bySelf: true
+    }
+  ])
 
   return (
     <AppLayout user={user} mutateUser={mutateUser}>
       <div className="min-h-screen bg-white mx-8 mt-8 py-12 px-12">
-        <div className="text-3xl font-bold">Browse your trainer</div>
-        <div className="text-2xl font-bold my-6">Filter</div>
-        <FilterTrainerList
-          preferenceFilter={preferenceFilter}
-          setPreferenceFilter={setPreferenceFilter}
-          sortBy={sortBy}
-          setSortBy={setSortBy}
-          sortType={sortType}
-          setSortType={setSortType}
-        />
-        <div className="text-2xl font-bold">Trainer List</div>
+        <div className="text-3xl font-bold">Chat</div>
+        <div className="text-2xl font-bold my-6">Chomtana Chanjaraswichai</div>
+        
         <List
-          dataSource={trainerList}
+          dataSource={messages}
           itemLayout="vertical"
-          renderItem={(item) => <TrainerListItem trainer={item} />}
+          renderItem={(item) => <ChatItem {...item} />}
         />
       </div>
     </AppLayout>
