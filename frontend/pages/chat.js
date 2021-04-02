38dc9db592533f410d4970164a7d6433fe0b7017
Chomtana <Chomtana001@gmail.com>
2021-04-02 22:41:17 +0700

    Add avatar

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -8,12 +8,21 @@ import { List } from 'antd';
 import { TrainerSortBy, TrainerSortType } from '../config/FilterTrainer.config';
 import { API_HOST } from '../config/config';
 import axios from 'axios';
+import Image from 'next/image';
 
 function ChatItem({message, avatar, at, bySelf}) {
   return (
     <div className={"flex " + (bySelf ? "justify-end" : "")}>
       <div className={"flex " + (bySelf ? "justify-end" : "")} style={{flexBasis: "75%"}}>
-        <div></div>
+        <div className="mr-2">
+          <Image
+            src={avatar}
+            width={36}
+            height={36}
+            layout="fixed"
+            className="rounded-full"
+          />
+        </div>
         <div className={"flex flex-col " + (bySelf ? "align-end" : "")}>
           <div className="p-2 bg-gray-200 rounded-xl">{message}</div>
           <div className="text-xs mt-1 text-gray-400">{at.toLocaleString()}</div>
