f1e31271689d90fe1794206ec3b7cb42cd3f809e
KKCORR <nattanon.hu@gmail.com>
2021-04-05 14:30:40 +0700

    ✨ implement hero section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,44 @@
 import useUser from '../lib/useUser';
+import Image from 'next/image';
+import Router from 'next/router';
 import { AppLayout } from '../components/common';
-import Loading from '../components/common/Loading';
+import { Button } from 'antd';
+
+const Hero = () => {
+  return (
+    <div className="relative">
+      <div>
+        <Image
+          src="/landing_hero.jpg"
+          layout="responsive"
+          width={760}
+          height={324}
+        />
+      </div>
+      <div className="absolute top-8 left-8">
+        <div className="font-bold text-5xl mb-3">
+          Find your best-fit workout training!
+        </div>
+        <div className="font-bold text-4xl text-gray-700 mb-6">at LetX</div>
+        <Button
+          type="primary"
+          size="large"
+          onClick={() => {
+            Router.push('/register');
+          }}
+        >
+          Register Now
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 const Landing = () => {
   const { user = {}, mutateUser } = useUser({ redirectTo: '/' });
   return (
     <AppLayout user={user} mutateUser={mutateUser}>
-      <Loading />
+      <Hero />
     </AppLayout>
   );
 };
