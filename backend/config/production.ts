7ac6e2204d928a1a784696ac6547c209a19c35f2
techid2000 <techid2000@gmail.com>
2021-04-25 20:09:46 +0700

    Setup production docker

--- backend/config/production.ts
+++ backend/config/production.ts
@@ -1,16 +1,16 @@
 export default {
   app: {
-    port: process.env.PROD_APP_PORT,
+    port: process.env.PROD_APP_PORT ?? 3000,
   },
   db: {
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_DATABASE,
-    synchronize: process.env.PROD_DB_SYNCHRONIZE,
+    host: process.env.PROD_DB_HOST ?? 'mysqldb',
+    port: process.env.PROD_DB_PORT ?? 3306,
+    username: process.env.PROD_DB_USERNAME ?? 'root',
+    password: process.env.PROD_DB_PASSWORD ?? 'admin',
+    database: process.env.PROD_DB_DATABASE ?? 'letx',
+    synchronize: process.env.PROD_DB_SYNCHRONIZE ?? false,
   },
   auth: {
-    jwtSecret: process.env.PROD_AUTH_JWT_SECRET,
+    jwtSecret: process.env.PROD_AUTH_JWT_SECRET ?? 'secret',
   },
 };
