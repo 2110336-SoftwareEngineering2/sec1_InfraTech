c39ef3f3bd3299ab7192cb20db8454d13082a6fb
Chomtana <Chomtana001@gmail.com>
2021-05-05 22:23:38 +0700

    Revert "Merge pull request #105 from 2110336-SoftwareEngineering2/backend/test/course"
    This reverts commit 0f674dbae341be0c443f7f0057a89c664fc0093b.

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,63 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RegisterModule } from 'src/register/register.module';
-import { LoginModule } from 'src/login/login.module';
-import { ProfileModule } from 'src/profile/profile.module';
-import { PreferenceModule } from 'src/preference/preference.module';
-import { TrainerModule } from 'src/trainer/trainer.module';
-import { CourseModule } from 'src/course/course.module';
-import { ApplicationModule } from 'src/application/application.module';
-import { FAQModule } from 'src/faq/faq.module';
-import { ReviewModule } from 'src/review/review.module';
-import { AppController } from 'src/app.controller';
-import { AppService } from 'src/app.service';
 
-import config from '../config/development';
-import { CourseService } from 'src/course/course.service';
-
-const dbConfig = config.db;
-
-describe('Course Module', () => {
+describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let courseService: CourseService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot({
-          // TODO: use configs
-          type: 'mysql',
-          host: dbConfig.host,
-          port: 3307,
-          username: dbConfig.username,
-          password: dbConfig.password,
-          database: dbConfig.database,
-          entities: ['dist/**/*.entity{.ts,.js}'],
-          synchronize: false,
-        }),
-        RegisterModule,
-        LoginModule,
-        ProfileModule,
-        PreferenceModule,
-        TrainerModule,
-        CourseModule,
-        ApplicationModule,
-        FAQModule,
-        ReviewModule,
-      ],
-      controllers: [AppController],
-      providers: [AppService],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    courseService = moduleFixture.get<CourseService>(CourseService);
   });
 
-  it('listCourses', async () => {
-    expect(await courseService.listCourses("user-id-1")).toEqual([])
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
   });
 });
